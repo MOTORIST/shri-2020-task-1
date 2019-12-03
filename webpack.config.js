6229b177339d38b3186cb04a70d1da9c199c8296
Gerasin Ivan <webartmotor@gmail.com>
2019-12-04 01:47:48 +0300

    build: build: installed and configured webpack
    Use 'yarn start' and 'yarn build' commands

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/js/index.js', './src/blocks/main.scss'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]/script.js?[contenthash]',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    index: 'index.html',
+    compress: false,
+    port: 9000,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+      inject: 'body',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              config: {
+                path: path.resolve(__dirname, 'postcss.config.js'),
+              },
+            },
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: {},
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({})],
+  },
+};
