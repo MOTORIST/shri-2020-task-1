4749dab5b439156ead8509a6b5404cee5b7bd805
Gerasin Ivan <webartmotor@gmail.com>
2019-12-07 15:36:19 +0300

    build: create dev and prod environment

---
+++ src/js/index.prod.js
@@ -0,0 +1,3 @@
+import initBlock from './initBlocks';
+
+initBlock();
--- webpack.config.js
+++ webpack.config.js
@@ -4,38 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: ['./src/js/index.js', './src/blocks/main.scss'],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name]/script.js?[contenthash]',
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    index: 'index.html',
-    compress: false,
-    port: 9000,
-    historyApiFallback: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html'),
-      filename: 'index.html',
-      inject: 'body',
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        useShortDoctype: true,
-      },
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-    new CopyPlugin([{ from: 'src/assets', to: 'assets' }]),
-  ],
+const common = {
   module: {
     rules: [
       {
@@ -69,3 +38,60 @@ module.exports = {
     minimizer: [new TerserPlugin({})],
   },
 };
+
+const dev = {
+  name: 'dev',
+  ...common,
+  ...{
+    entry: ['./src/js/index.js', './src/blocks/main.scss'],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'script.js?[contenthash]',
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      index: 'index.html',
+      compress: false,
+      port: 9000,
+      historyApiFallback: true,
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+      inject: 'body',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new CopyPlugin([{ from: 'src/assets', to: 'assets' }]),
+  ],
+};
+
+const build = {
+  name: 'build',
+  ...common,
+  ...{
+    entry: ['./src/js/index.prod.js', './src/blocks/main.scss'],
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'script.js?[contenthash]',
+    },
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+};
+
+module.exports = [dev, build];
